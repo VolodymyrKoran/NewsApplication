f1cf8b9062372cf1c22ab0068e5a45bde3e7812d
Volodymyr Koran <vkoran05@ukr.net>
2023-12-12 14:42:37 -0500

    f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,24 @@ android {
 
 dependencies {
 
+    val appcompat_version = "1.6.1"
+
+    implementation("androidx.appcompat:appcompat:$appcompat_version")
+    // For loading and tinting drawables on older versions of the platform
+    implementation("androidx.appcompat:appcompat-resources:$appcompat_version")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // For control over item selection of both touch and mouse driven selection
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.8")
+
+
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
