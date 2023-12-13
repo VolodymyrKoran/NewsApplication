602265f23162d7bfe38b45331e5629603e19e707
Volodymyr Koran <vkoran05@ukr.net>
2023-12-13 14:43:04 -0500

    f

--- app/src/main/java/com/example/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 89259e6d9b144a33b1c34c77454f711a
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
