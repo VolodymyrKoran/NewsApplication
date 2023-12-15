9be29ca42fe35b65ae1e29c0e7424980a5bbe065
Volodymyr Koran <vkoran05@ukr.net>
2023-12-15 13:49:32 -0500

    f

---
+++ app/src/main/java/com/example/newsapplication/NewsDeatilActivity.java
@@ -0,0 +1,16 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsDeatilActivity extends AppCompatActivity {
+
+    String title,desc,content,imageURL,url;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_deatil);
+        title = getIntent().getStringExtra('')
+    }
+}
